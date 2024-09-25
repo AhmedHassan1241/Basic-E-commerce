067ccf37595488de1111cf48a7c65a8a6a6f7c5a
Ahmed Hassan <93686680+AhmedHassan1241@users.noreply.github.com>
2024-09-25 20:49:02 +0300

    "trying deploy"

--- src/Components/MySlider.jsx
+++ src/Components/MySlider.jsx
@@ -1,4 +1,6 @@
-
+import img1 from "../images/hands-coffee-cup-apple.jpg"
+import img2 from "../images/html_image_slider.jpg"
+import img3 from "../images/1544124707023.jpeg"
 const MySlider =()=> {
  
     return (
@@ -6,13 +8,13 @@ const MySlider =()=> {
       <div id="carouselExampleAutoplaying" className="carousel slide container" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active">
-    <img src={"/images/hands-coffee-cup-apple.jpg"} className="d-block w-100" alt="..." style={{height:"578px",width:"477px"}}/>
+    <img src={img1} className="d-block w-100" alt="..." style={{height:"578px",width:"477px"}}/>
     </div>
     <div className="carousel-item">
-    <img src="/images/1544124707023.jpeg" className="d-block w-100" alt="laptop" style={{height:"578px",width:"477px"}}/>
+    <img src={img2}className="d-block w-100" alt="laptop" style={{height:"578px",width:"477px"}}/>
     </div>
     <div className="carousel-item">
-      <img src="/images/html_image_slider.jpg" className="d-block w-100" alt="..." style={{height:"578px",width:"477px"}}/>
+      <img src={img3} className="d-block w-100" alt="..." style={{height:"578px",width:"477px"}}/>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
